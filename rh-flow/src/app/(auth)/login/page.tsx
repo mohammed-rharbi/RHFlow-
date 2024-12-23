642f0a96931e2c4eb8247a7a2736ac3bc435487a
mohammed-rharbi <rharbi383@gmail.com>
2024-12-23 17:04:45 +0100

    build : setUp the project env and structure the project folders

---
+++ rh-flow/src/app/(auth)/login/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+
+<div className="flex justify-center items-center h-screen">
+
+<div className="w-80 rounded-2xl bg-slate-900">
+  <div className="flex flex-col gap-2 p-8">
+    <p className="text-center text-3xl text-gray-300 mb-4">Login</p>
+
+    <input className="bg-slate-900 w-full rounded-lg border border-gray-300 px-4 py-3 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 focus:ring-offset-gray-800" placeholder="Email"/>
+    <input className="bg-slate-900 w-full rounded-lg border border-gray-300 px-4 py-3 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 focus:ring-offset-gray-800" placeholder="Password"/>
+ 
+    <button className="inline-block cursor-pointer rounded-md bg-gray-700 px-4 py-3.5 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-800 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-700 focus-visible:ring-offset-2 active:scale-95">Login with google</button>
+
+    <button className="inline-block cursor-pointer rounded-md bg-gray-700 px-4 py-3.5 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-800 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-700 focus-visible:ring-offset-2 active:scale-95">Login</button>
+  </div>
+</div>
+</div>
+
+
+  )
+}
+
