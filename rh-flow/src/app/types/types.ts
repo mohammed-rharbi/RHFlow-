95fa8090443350a97c19e0696951086bb2dcead5
mohammed-rharbi <rharbi383@gmail.com>
2024-12-27 10:23:00 +0100

    Add type definitions

---
+++ rh-flow/src/app/types/types.ts
@@ -0,0 +1,17 @@
+
+
+export interface Users {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    role: string;
+}
+
+export interface LoginUser{
+    
+    email: string;
+    password: string;
+}
+
+
