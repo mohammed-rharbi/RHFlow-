28d49cbc3906641c0b41090d99076d747111d4a5
mohammed-rharbi <rharbi383@gmail.com>
2024-12-27 10:22:47 +0100

    Update home page

--- rh-flow/src/app/home/page.tsx
+++ rh-flow/src/app/home/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Link from 'next/link';
 
 export default function Home() {
   return (
-
-    
-    <div>page</div>
+    <div>
+      <h1>Home</h1>
+    </div>
   )
 }
